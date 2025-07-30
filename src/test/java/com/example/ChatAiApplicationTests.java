c0959e17c8ca51987f9fed0ab3b905dce5dc7ea5
dejvis06 <dejvis.beqiraj@fullscreendigital.ro>
2025-07-30 18:39:27 +0200

    Initial commit

---
+++ src/test/java/com/example/ChatAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
